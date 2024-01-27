57d89f16f3db495b4364396fd338cdf12758d1eb
DINANJANA B.N <105732109+L4Y3R@users.noreply.github.com>
2024-01-27 11:39:59 +0530

    add button modified

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
 
 import Recents from "./recent/page";
 import All from "./all/page";
@@ -51,14 +58,55 @@ const Home: React.FC<HomeProps> = () => {
       {activeTab === "allMovies" && <All />}
 
       <div className="fixed right-8 bottom-16 md:right-20 z-10">
-        <button>
-          <svg
-            className="w-14 h-14 fill-white transform hover:scale-125 transition-transform duration-300"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512">
-            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
-          </svg>
-        </button>
+        <Dropdown>
+          <DropdownTrigger>
+            <button>
+              <svg
+                className="w-14 h-14 fill-white transform hover:scale-125 transition-transform duration-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512">
+                <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
+              </svg>
+            </button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Static Actions"
+            className=" rounded-xl bg-darkCyan p-2">
+            <DropdownItem key="movie" className="mb-2">
+              <Link href="/add-movie">
+                <div className="flex items-center gap-2">
+                  <div>Add a Movie</div>
+
+                  <div>
+                    <Image
+                      src="icons/film-solid.svg"
+                      alt="movie icon"
+                      height={20}
+                      width={20}
+                    />
+                  </div>
+                </div>
+              </Link>
+            </DropdownItem>
+
+            <DropdownItem key="tv">
+              <Link href="/add-tv">
+                <div className="flex items-center gap-2">
+                  <div>Add a TV Series</div>
+
+                  <div>
+                    <Image
+                      src="/icons/tv-solid.svg"
+                      alt="tv icon"
+                      height={20}
+                      width={20}
+                    />
+                  </div>
+                </div>
+              </Link>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     </div>
   );
--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function NavBar() {
   return (
     <div className="w-full h-14 bg-darkCyan flex items-center px-5 md:px-20 lg:px-44 justify-between">
-      <div className="flex items-center md:gap-3">
-        <Image
-          src="/logo/affiliatetheme.svg"
-          alt="logo"
-          width={25}
-          height={25}
-          className="pb-2"
-        />
-        <span className="font-Montserrat font-bold text-lg">Cinephile</span>
-      </div>
+      <Link href="/">
+        <div className="flex items-center md:gap-3">
+          <Image
+            src="/logo/affiliatetheme.svg"
+            alt="logo"
+            width={25}
+            height={25}
+            className="pb-2"
+          />
+          <span className="font-Montserrat font-bold text-lg">Cinephile</span>
+        </div>
+      </Link>
       <div>
         <button className="rounded-lg px-2 py-1 bg-teal-500 hover:bg-teal-800 transition-transform-background duration-500">
           Sign Out
