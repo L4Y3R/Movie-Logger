a8a0754e0af772215076f6ba143a2ae503ee1ed2
DINANJANA B.N <105732109+L4Y3R@users.noreply.github.com>
2024-01-28 16:01:28 +0530

    fetch tv shows and movies

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const movieRouter = require('./routes/movies')
-const tvRouter = require('./routes/tv')
+const movieRouter = require("./routes/movies");
+const tvRouter = require("./routes/tv");
 
-const app = express()
+const app = express();
 
 //middleware
-app.use(express.json())
-app.use((req, res, next) =>{
-    console.log(req.path, req.method)
-    next()
-}) 
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/movies', movieRouter)
-app.use('/api/tv', tvRouter)
+app.use("/api/movies", movieRouter);
+app.use("/api/tv", tvRouter);
 
 //connect to db
-mongoose.connect(process.env.MONGO)
-    .then(()=>{
-        app.listen(process.env.PORT, ()=>{
-            console.log("Connected to db and running at port:", process.env.PORT)
-        })
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db and running at port:", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- frontend/app/all-movies/page.jsx
+++ frontend/app/all-movies/page.jsx
@@ -1,22 +1,31 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Search from "../../components/SearchBar";
+import { useState, useEffect } from "react";
 
 export default function allMovies() {
-  const cardData = [
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-    { date: "2024-03-02", title: "Interstellar", imageSrc: "/test/nolan.jpg" },
-  ];
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/api/movies");
+        const json = await response.json();
+        console.log("Received Data", json);
+
+        if (response.ok) {
+          setMovies(json);
+        }
+      } catch (error) {
+        console.error("Error fetching movies", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <>
@@ -29,28 +38,27 @@ export default function allMovies() {
         </div>
 
         <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6 gap-y-5 md:gap-y-10">
-          {cardData.map((card, index) => (
-            <Link href="/info-movie">
-              <div
-                key={index}
-                className="bg-slate-200 h-62 w-40 md:h-64 md:w-44 lg:h-80 lg:w-52 rounded-xl border-2 border-darkCyan transform hover:scale-105 transition-transform duration-300">
-                <div className="bg-gradient-to-b rounded-t-xl from-slate-900 to-transparent absolute top-0 left-0 w-full h-full"></div>
-                <h7 className="absolute top-2 left-2 text-white text-xs">
-                  {card.date}
-                </h7>
-                <h3 className="absolute top-5 left-2 text-white">
-                  {card.title}
-                </h3>
-                <Image
-                  src={card.imageSrc}
-                  alt={`Card Image - ${index}`}
-                  className="w-full h-full object-cover rounded-xl"
-                  width={1000}
-                  height={1000}
-                />
-              </div>
-            </Link>
-          ))}
+          {movies &&
+            movies.map((card, index) => (
+              <Link key={index} href="/info-movie">
+                <div className="bg-slate-200 h-62 w-40 md:h-64 md:w-44 lg:h-80 lg:w-52 rounded-xl border-2 border-darkCyan transform hover:scale-105 transition-transform duration-300">
+                  <div className="bg-gradient-to-b rounded-t-xl from-slate-900 to-transparent absolute top-0 left-0 w-full h-full"></div>
+                  <h6 className="absolute top-2 mb-2 left-2 text-white text-xs">
+                    {new Date(card.createdAt).toLocaleDateString()}
+                  </h6>
+                  <h3 className="absolute top-5 left-2 text-white">
+                    {card.title}
+                  </h3>
+                  <Image
+                    src={card.imageSrc}
+                    alt={`Card Image - ${index}`}
+                    className="w-full h-full object-cover rounded-xl"
+                    width={1000}
+                    height={1000}
+                  />
+                </div>
+              </Link>
+            ))}
         </div>
       </div>
     </>
